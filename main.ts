158a4d802424ebf90ec133a24bff3d22920e84db
piratmartinek <121960967+piratmartinek@users.noreply.github.com>
2023-01-08 12:34:38 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,32 @@
-let strip = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
+input.onButtonPressed(Button.A, function () {
+    strip.showRainbow(1, 360)
+    for (let index = 0; index < 25; index++) {
+        strip.rotate(1)
+        strip.show()
+        basic.pause(100)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    strip.setPixelColor(number - 1, neopixel.colors(NeoPixelColors.Blue))
+    basic.showString("" + (number))
+    basic.pause(2000)
+    strip.clear()
+    number = randint(1, 8)
+})
+input.onButtonPressed(Button.B, function () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+})
+let number = 0
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
 strip.showRainbow(1, 360)
+strip.setBrightness(100)
+let range = strip.range(0, 8)
+number = randint(1, 8)
+basic.showString("" + (number))
+basic.forever(function () {
+    strip.setBrightness(input.soundLevel())
+    for (let index = 0; index <= 8; index++) {
+        strip.show()
+    }
+})
